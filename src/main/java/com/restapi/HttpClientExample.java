619e188cb9a1a05ce1c549a2ddd06565ece43ade
deepali-gaurkar <deepaligaurkar29@gmail.com>
2025-07-24 16:05:55 +0100

    changed stream logic

--- src/main/java/com/restapi/HttpClientExample.java
+++ src/main/java/com/restapi/HttpClientExample.java
@@ -12,91 +12,6 @@ import java.util.stream.Collectors;
 
 public class HttpClientExample {
 
-    static class Data{
-
-        String name;
-
-        public Data() {
-
-        }
-
-        public String getGenre() {
-            return genre;
-        }
-
-        public void setGenre(String genre) {
-            this.genre = genre;
-        }
-
-        public Double getImdb_rating() {
-            return imdb_rating;
-        }
-
-        public void setImdb_rating(Double imdb_rating) {
-            this.imdb_rating = imdb_rating;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        String runtime_of_series;
-        String genre;
-        Double imdb_rating;
-
-    }
-
-    static class Api{
-        int page;
-        int per_page;
-        List<Data> data;
-
-        public int getPage() {
-            return page;
-        }
-
-        public void setPage(int page) {
-            this.page = page;
-        }
-
-        public int getPer_page() {
-            return per_page;
-        }
-
-        public void setPer_page(int per_page) {
-            this.per_page = per_page;
-        }
-
-        public List<Data> getData() {
-            return data;
-        }
-
-
-        public int getTotal() {
-            return total;
-        }
-
-        public void setTotal(int total) {
-            this.total = total;
-        }
-
-        public int getTotal_pages() {
-            return total_pages;
-        }
-
-        public void setTotal_pages(int total_pages) {
-            this.total_pages = total_pages;
-        }
-
-        int total;
-        int total_pages;
-
-    }
-
     private void bestInGenre(String genre) throws IOException, InterruptedException {
 
         int page=1;
@@ -115,7 +30,7 @@ public class HttpClientExample {
             String response = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
 
             Gson gson = new Gson();
-            Api api = gson.fromJson(response,Api.class);
+            ApiResponse api = gson.fromJson(response,ApiResponse.class);
 
 
 
@@ -148,7 +63,7 @@ public class HttpClientExample {
        String string=  allShows.stream()
                 .filter(data -> data.genre!=null && data.genre.toLowerCase()
                 .contains(genre.toLowerCase()))
-               .min(Comparator.comparingDouble(Data::getImdb_rating).reversed().thenComparing(Data::getName))
+               .max(Comparator.comparingDouble(Data::getImdb_rating).thenComparing(Data::getName))
                .map(Data::getName)
               .orElse("No names found");;
 
@@ -157,11 +72,8 @@ public class HttpClientExample {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClientExample httpClientExample = new HttpClientExample();
-        httpClientExample.bestInGenre("Comedy");
+        httpClientExample.bestInGenre("Thriller");
 
     }
-
-
-
 
 }
